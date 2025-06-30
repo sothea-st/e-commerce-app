df46b50c4b26c691124f4360f8ab30be5f765222
sothea-st <sotheakam210@gmail.com>
2025-06-30 08:19:11 +0700

    add new

---
+++ services/notification/src/main/java/com/alibou/notification/notification/Notification.java
@@ -0,0 +1,30 @@
+package com.alibou.notification.notification;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Document
+public class Notification {
+
+    @Id
+    private String id;
+    private NotificationType type;
+    private LocalDateTime notificationDate;
+    private com.alibou.notification.kafka.order.OrderConfirmation orderConfirmation;
+    private com.alibou.notification.kafka.payment.PaymentConfirmation paymentConfirmation;
+}
